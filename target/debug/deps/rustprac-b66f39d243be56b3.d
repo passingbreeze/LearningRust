d521e2e472a79b7da14c060f4ea9e26f7130c858
thomashong <jungmin1237@pusan.c.kr>
2019-12-02 16:16:06 +0900

    Beginning to learn Rust!

---
+++ target/debug/deps/rustprac-b66f39d243be56b3.d
@@ -0,0 +1,5 @@
+/Users/thomashong/Desktop/Coding/rustprac/target/debug/deps/rustprac-b66f39d243be56b3: src/main.rs
+
+/Users/thomashong/Desktop/Coding/rustprac/target/debug/deps/rustprac-b66f39d243be56b3.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rustprac.d
@@ -0,0 +1,1 @@
+/Users/thomashong/Desktop/Coding/rustprac/target/debug/rustprac: /Users/thomashong/Desktop/Coding/rustprac/src/main.rs
